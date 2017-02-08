06d866609c0103a58af34899db46706c4d2cdfe0
Greg Lee <gml2136@gmail.com>
2017-02-07 21:30:07 -0800

    still working on it

--- assets/javascript/starwars.js
+++ assets/javascript/starwars.js
@@ -1,7 +1,8 @@
 //create globals
 
-//define character objects
 
+//create a function to create objects for the game. parameters are for
+//both display and game functionality.
 function Character(name, strength, counter, health, color, imagePath){
   this.name = name;
   this.strength = strength;
@@ -11,12 +12,6 @@ function Character(name, strength, counter, health, color, imagePath){
   this.imagePath = imagePath;
 
   //methods
-  this.attack = function(target){
-    target.health -= this.strength;
-    this.strength += 6;
-    this.health -= target.counter;
-  };
-
   this.isDead = function(){
     if(this.health > 0){
       return false;
@@ -26,6 +21,7 @@ function Character(name, strength, counter, health, color, imagePath){
   };
 }
 
+//define character objects and push them into the characterArray
 var characterArray = [];
 var mace = new Character("Mace Windu", 10, 5, 100, "purple", "assets/images/mace.jpg");
 characterArray.push(mace);
@@ -33,16 +29,28 @@ var vader = new Character("Darth Vader", 13, 7, 120, "red", "assets/images/vader
 characterArray.push(vader);
 var yoda = new Character("Yoda ", 12, 10, 110, "green", "assets/images/yoda.jpg");
 characterArray.push(yoda);
-var grievous = new Character("General Grievous", 10, 4, 95, "gray", "assets/images/grievous.jpg");
-characterArray.push(grievous);
+// var grievous = new Character("General Grievous", 10, 4, 95, "gray", "assets/images/grievous.jpg");
+// characterArray.push(grievous);
 // var rey = new Character("Rey ", 11, 6, 105, "blue", "assets/images/rey.jpg");
 // characterArray.push(rey);
 
-
+//initialize empty objects, these will be overridden with the appropriate
+//character object when the click events execute.
 var playerObj = {};
 var opponentObj = {};
 
-var  createCharacters = function(currentValue){
+var attack = function(agent, patient){
+  patient.health -= agent.strength;
+  agent.strength += 6;
+  agent.health -= patient.counter;
+};
+
+
+
+//this function is designed to be run with characterArray.map(***).
+//it uses some JQ methods to make them unique to each character.
+//it also creates divs for each of the characters and appends them to the #choices section.
+var createCharacters = function(currentValue){
       var ch = $("<div>");
       var id = currentValue.name.substr(0, currentValue.name.indexOf(" "));
       ch.attr("id", id);
@@ -55,6 +63,15 @@ var  createCharacters = function(currentValue){
       $("#choices").append(ch);
   }
 
+
+/* This function is designed to run within the array.map() method.
+* the array is an array of character objects, the same array as before.
+* It takes the divs created by createCharacters() and adds click events
+* for player selection. It then moves the player to the #player section
+* adds the rest to the #enemies section, then removes the player choice click events
+* via $().off("click"). Then it will add the opponent click events which moves
+* the ".character" div to the #opponent section. phew - thats a lot.
+*/
 var addPickClicks = function(currentValue){
     var id = currentValue.name.substr(0, currentValue.name.indexOf(" "));// reusing, not a fan of this
     var hashid = "#" + id;
@@ -71,7 +88,7 @@ var addPickClicks = function(currentValue){
         $(this).appendTo(opponent);
         getOpponentObj();
         play(playerObj, opponentObj);
-        enemies.off();
+        enemies.off("click");
         });
       });
 }
@@ -95,24 +112,35 @@ var getOpponentObj = function(){
   opponentObj = n[0];
 }
 
+var counter = 0;
+
 var play = function(player, opponent){
+  counter++;
+  console.log("ran play "+counter+" times")
   var JQplayer = $("#player > div");
-  var button = $("<button>")
-  button.html("Attack")
+  var button = $("<button>");
+  button.html("Attack");
   JQplayer.append(button);
+  $("#opponent").off("click");
   button.on("click", function(){
-    if (player.strength >= opponent.health){
-      player.attack(opponent);
-      $("#opponent > div").remove();
-      nextOpponentClick();
-      updateHealthData(player, opponent);
-      button.remove();
-    } else if(!player.isDead() && !opponent.isDead()){
-      player.attack(opponent);
-      updateHealthData(player,opponent);
-    }
+    battle(player, opponent);
   });
 }
+
+var battle = function(player, opponent){
+  if (player.strength >= opponent.health){
+    attack(player, opponent);
+    $("#opponent > div").remove();
+    nextOpponentClick();
+    updateHealthData(player, opponent);
+    $("button").remove();
+    console.log("ran button.remove");
+  } else if(!player.isDead() && !opponent.isDead()){
+    attack(player, opponent);
+    updateHealthData(player, opponent);
+    console.log("ran keep playing");
+  }
+};
 
 var nextOpponentClick = function(){
   $("#enemies > div").on("click", function(){
@@ -121,7 +149,9 @@ var nextOpponentClick = function(){
     getOpponentObj();
     play(playerObj, opponentObj);
   });
-}
+};
+
+//still to do, find out a better way for DOM logic. Need a game end scenario.
 
 
 characterArray.map(createCharacters);
